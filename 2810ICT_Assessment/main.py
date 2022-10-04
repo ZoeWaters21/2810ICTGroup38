a91f179e179a3c3d826a7dd271787051e1785c45
L0adingGit <110453209+L0adingGit@users.noreply.github.com>
2022-10-04 19:51:03 +1000

    Some GUI implemented

--- 2810ICT_Assessment/main.py
+++ 2810ICT_Assessment/main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import wx
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+class GameGUI(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(540, 280))
+        self.initialise()
+
+    def initialise(self):
+
+        pnl = wx.Panel(self)
+        rows = wx.BoxSizer(wx.VERTICAL)
+        head = wx.StaticText(pnl, label="New South Wales Traffic Penalty Analysis")
+        font = head.GetFont()  # get the standard font
+        font.PointSize += 10  # increases the size
+        font = font.Bold()  # makes it bold
+        head.SetFont(font)  # resets the font
+        rows.Add(head, 1, wx.ALIGN_CENTER | wx.BOTTOM, border=2)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        Navigation = wx.BoxSizer(wx.VERTICAL)
+        NavHead = wx.StaticText(pnl, label="Queries")
+        DateRangeButton = wx.Button(pnl, label = "Date Range")
+        OffenceCodeButton = wx.Button(pnl, label = "Offence Code")
+        RadarCamButton = wx.Button(pnl, label = "Radar/Camera")
+        MobilePhoneButton = wx.Button(pnl, label = "Date Range")
+        CustomQueryButton = wx.Button(pnl, label = "Date Range")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        Navigation.Add(NavHead,1,wx.ALIGN_LEFT|wx.LEFT, border = 50)
+        Navigation.Add(DateRangeButton,1,wx.ALIGN_LEFT|wx.LEFT, border = 50)
+        Navigation.Add(OffenceCodeButton, 1, wx.ALIGN_LEFT | wx.LEFT, border=50)
+        Navigation.Add(RadarCamButton, 1, wx.ALIGN_LEFT | wx.LEFT, border=50)
+        Navigation.Add(MobilePhoneButton, 1, wx.ALIGN_LEFT | wx.LEFT, border=50)
+        Navigation.Add(CustomQueryButton, 1, wx.ALIGN_LEFT | wx.LEFT, border=50)
+        rows.Add(Navigation, 1, wx.ALIGN_LEFT)
+
+
+        pnl.SetSizerAndFit(rows)
+        self.Show(True)
+
+
+app = wx.App()
+frame = GameGUI(None, "New South Wales Traffic Penalty Analysis")
+app.MainLoop()
