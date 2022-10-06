5d6da34a8a07eb0fd658e99e8182be0fe580f1b8
ZoeWaters21 <110453020+ZoeWaters21@users.noreply.github.com>
2022-10-06 19:22:13 +1000

    Add files via upload

--- 2810ICT_Assessment/Search.py
+++ 2810ICT_Assessment/Search.py
@@ -1,10 +1,16 @@
 import pandas as pd
 
-dataFrame = pd.read_csv('penalty_data_set_2.csv')
+df = pd.read_csv('penalty_data_set_2.csv', low_memory=False, parse_dates=['OFFENCE_MONTH'], dayfirst=True)
 
 def dateRange(beginDate, endDate):
-    dateRangeFrame = pd.dataFrame.period_range(column = "OFFENCE_MONTH", start=beginDate, end=endDate)
-    dateRangeFrame = pd.loc[dateRangeFrame]
-    return dateRangeFrame
+    df['OFFENCE_MONTH'] = pd.to_datetime(df['OFFENCE_MONTH'])
+    df2 = df[(df['OFFENCE_MONTH'] > beginDate) & (df['OFFENCE_MONTH'] < endDate)]
+    return df2
 
-print(dateRange('01/01/2012', '01/01/2013'))
+def customSearch(searchWord):
+
+    df2 = df[df["OFFENCE_DESC"].str.lower().str.contains(searchWord)]
+    return df2["OFFENCE_DESC"]
+
+#print(dateRange('01-01-11', '01-01-13'))
+print(customSearch('nude'))
