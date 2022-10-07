8552e4227b42c64169c7eade0159e83c8d2312c9
ZoeWaters21 <110453020+ZoeWaters21@users.noreply.github.com>
2022-10-07 12:02:01 +1000

    Add files via upload

--- 2810ICT_Assessment/Search.py
+++ 2810ICT_Assessment/Search.py
@@ -12,5 +12,36 @@ def customSearch(searchWord):
     df2 = df[df["OFFENCE_DESC"].str.lower().str.contains(searchWord)]
     return df2["OFFENCE_DESC"]
 
+def offenceCodeDistribution():
+    columns = df.values.tolist()
+    offences = []
+    for i in columns:
+        tuple = [i[2], i[23]]
+        offences.append(tuple)
+
+    count = {}
+    runtime = 0
+    for list in offences:
+        runtime += 1
+        print(runtime)
+        for elements in offences:
+            if elements[0] in count:
+                count[elements[0]] += elements[1]
+                print(count)
+                runtime += 1
+                print(runtime)
+                if runtime > 10:
+                    break
+            else:
+                count[elements[0]] = elements[1]
+                print(count)
+                runtime += 1
+                print(runtime)
+                if runtime > 10:
+                    break
+
+    return count
+
 #print(dateRange('01-01-11', '01-01-13'))
-print(customSearch('nude'))
+#print(customSearch('nude'))
+print(offenceCodeDistribution())
