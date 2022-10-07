739102350c504ce9c2c22ac1f75027a33754ec1a
ZoeWaters21 <110453020+ZoeWaters21@users.noreply.github.com>
2022-10-08 06:47:09 +1000

    Add files via upload

--- 2810ICT_Assessment/Search.py
+++ 2810ICT_Assessment/Search.py
@@ -12,38 +12,46 @@ def customSearch(searchWord):
     df2 = df[df["OFFENCE_DESC"].str.lower().str.contains(searchWord)]
     return df2["OFFENCE_DESC"]
 
-def offenceCodeDistribution():
-    columns = df.values.tolist()
-    offences = []
-    for i in columns:
-        tuple = [i[2], i[23]]
-        offences.append(tuple)
 
-    count = {}
-    runtime = 0
-    for list in offences:
-        runtime += 1
-        print(runtime)
-        if runtime > 10:
-            break
-        for elements in offences:
-            if elements[0] in count:
-                count[elements[0]] += elements[1]
-                print(count)
-                runtime += 1
-                print(runtime)
-                if runtime > 10:
-                    break
+def dateRangeOffenceCode(beginDate, endDate):
+    df['OFFENCE_MONTH'] = pd.to_datetime(df['OFFENCE_MONTH'])
+    df2 = df[(df['OFFENCE_MONTH'] > beginDate) & (df['OFFENCE_MONTH'] < endDate)]
+    df3 = df2["OFFENCE_CODE"].value_counts()
+    offenceCode = df3.head(19)
+    return offenceCode
+
+def mobiletrendSearch():
+    distributionDic = {}
+    df2 = df[df["OFFENCE_DESC"].str.lower().str.contains("mobile phone")]
+
+    df2['OFFENCE_MONTH'] = pd.to_datetime(df2['OFFENCE_MONTH']).dt.year
+    df3 = df2["OFFENCE_CODE"].value_counts()
+    print(df3)
+    years = df2['OFFENCE_MONTH'].unique()
+    years.sort()
+
+    topCases = df3.index.tolist()
+    caseList = []
+    i = 0
+    for year in years:
+        ydf = df2[df2['OFFENCE_MONTH'].astype('str').str.contains(str(year))]
+        for case in topCases:
+            cdf = ydf[ydf["OFFENCE_CODE"].astype('str').str.contains(str(case))]
+            ldf = cdf["OFFENCE_CODE"].value_counts()
+            if ldf.empty:
+                tldf = 0
             else:
-                count[elements[0]] = elements[1]
-                print(count)
-                runtime += 1
-                print(runtime)
-                if runtime > 10:
-                    break
+                tldf = ldf.tolist()
+                tldf = tldf[0]
+            caseList.append([case, tldf])
+        distributionDic[year] = caseList[i*9: + (9+(9*i))]
+        i += 1
 
-    return count
+    return distributionDic
+
 
 #print(dateRange('01-01-11', '01-01-13'))
 #print(customSearch('nude'))
-print(offenceCodeDistribution())
+#print(dateRangeOffenceCode("01-01-2012", '01-01-2013'))
+
+print(mobiletrendSearch())
