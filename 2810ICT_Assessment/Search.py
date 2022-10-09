530122dfd1a0258f0f1bbd9c6ae65e63c4e5f677
ZoeWaters21 <110453020+ZoeWaters21@users.noreply.github.com>
2022-10-09 14:49:39 +1000

    Add files via upload

--- 2810ICT_Assessment/Search.py
+++ 2810ICT_Assessment/Search.py
@@ -2,24 +2,6 @@ import pandas as pd
 
 df = pd.read_csv('penalty_data_set_2.csv', low_memory=False, parse_dates=['OFFENCE_MONTH'], dayfirst=True)
 
-def dateRange(beginDate, endDate):
-    df['OFFENCE_MONTH'] = pd.to_datetime(df['OFFENCE_MONTH'])
-    df2 = df[(df['OFFENCE_MONTH'] > beginDate) & (df['OFFENCE_MONTH'] < endDate)]
-    return df2
-
-def customSearch(searchWord):
-
-    df2 = df[df["OFFENCE_DESC"].str.lower().str.contains(searchWord)]
-    return df2["OFFENCE_DESC"]
-
-
-def dateRangeOffenceCode(beginDate, endDate):
-    df['OFFENCE_MONTH'] = pd.to_datetime(df['OFFENCE_MONTH'])
-    df2 = df[(df['OFFENCE_MONTH'] > beginDate) & (df['OFFENCE_MONTH'] < endDate)]
-    df3 = df2["OFFENCE_CODE"].value_counts()
-    offenceCode = df3.head(19)
-    return offenceCode
-
 def mobiletrendSearch():
     distributionDic = {}
     df2 = df[df["OFFENCE_DESC"].str.lower().str.contains("mobile phone")]
@@ -48,10 +30,4 @@ def mobiletrendSearch():
         i += 1
 
     return distributionDic
-
-
-#print(dateRange('01-01-11', '01-01-13'))
-#print(customSearch('nude'))
-#print(dateRangeOffenceCode("01-01-2012", '01-01-2013'))
-
 print(mobiletrendSearch())
